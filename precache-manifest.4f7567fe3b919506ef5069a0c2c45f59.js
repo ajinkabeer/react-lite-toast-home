835f918c229feb86e3d8d7ed2307f45cb652c5e8
Ajin Kabeer <ajiinkabeer@gmail.com>
2020-05-18 10:26:53 +0530

    Updates

---
+++ precache-manifest.4f7567fe3b919506ef5069a0c2c45f59.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39bd4892a5c13956c796483ff3bc0588",
+    "url": "/react-lite-toast-home/index.html"
+  },
+  {
+    "revision": "9a272b475044ef505ee9",
+    "url": "/react-lite-toast-home/static/css/2.a2204ab7.chunk.css"
+  },
+  {
+    "revision": "0a31dd8c099657033a6f",
+    "url": "/react-lite-toast-home/static/css/main.075dd1a0.chunk.css"
+  },
+  {
+    "revision": "9a272b475044ef505ee9",
+    "url": "/react-lite-toast-home/static/js/2.1328218e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-lite-toast-home/static/js/2.1328218e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a31dd8c099657033a6f",
+    "url": "/react-lite-toast-home/static/js/main.84c8a49b.chunk.js"
+  },
+  {
+    "revision": "48f88cf34739b8001ad2",
+    "url": "/react-lite-toast-home/static/js/runtime-main.a1b3dbcf.js"
+  },
+  {
+    "revision": "b345f2548310b24f367b2971613ab114",
+    "url": "/react-lite-toast-home/static/media/check~YoXmxPYW.b345f254.svg"
+  },
+  {
+    "revision": "516ae887052d49e4bcc1547c5164290b",
+    "url": "/react-lite-toast-home/static/media/close~MmoXfehv.516ae887.svg"
+  },
+  {
+    "revision": "12333ffe8a17ede83c2d63ec75cc1a45",
+    "url": "/react-lite-toast-home/static/media/error~sZYKhomM.12333ffe.svg"
+  },
+  {
+    "revision": "e4cf123d407f6d678330c8caebc8587f",
+    "url": "/react-lite-toast-home/static/media/info~hXvLIqUM.e4cf123d.svg"
+  },
+  {
+    "revision": "184b15534b3a1a389febf7cde782f61e",
+    "url": "/react-lite-toast-home/static/media/warning~vKUgrCLp.184b1553.svg"
+  }
+]);
