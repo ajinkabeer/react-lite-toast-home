090f3383a2b9e2e2229b6c774ad8290c8eb9966c
Ajin Kabeer <ajiinkabeer@gmail.com>
2020-05-19 11:52:42 +0530

    Bump react lite toast version

--- src/App.js
+++ src/App.js
@@ -11,12 +11,14 @@ function App() {
   const [position, setPosition] = useState('')
   const [title, setTitle] = useState('')
   const [message, setMessage] = useState('')
+  const [duration, setDuration] = useState(1500)
 
-  const toastHandler = (type, position, title, message) => {
+  const toastHandler = (type, position, title, message, duration) => {
     setToastType(type)
     setPosition(position)
     setTitle(title)
     setMessage(message)
+    setDuration(duration)
     setToast(!toast)
   }
 
@@ -28,11 +30,12 @@ function App() {
         <a href="https://www.github.com/ajinkabeer/react-lite-toast" target="_blank" rel="noopener noreferrer"><img className="logo-img" src={github} alt="github" /></a>
         <a href="https://www.npmjs.com/package/react-lite-toast" target="_blank" rel="noopener noreferrer"><img className="logo-img" src={npm} alt="npm" /></a>
         <p>A minimal toast notification for react with bundled size of 3 kB.</p>
-        <button class="btn success" onClick={() => toastHandler('success', 'bottomup', 'Success', 'Dunder Mifflin')}>success</button>
-        <button class="btn error" onClick={() => toastHandler('error', 'topdown', 'Failed', 'Battlestart Galatica')}>error</button>
-        <button class="btn warning" onClick={() => toastHandler('warning', 'bottomright', 'Warning', 'Wooooof')} >warning</button>
-        <button class="btn info" onClick={() => toastHandler('info', 'bottomleft', 'Info', 'Please note')}>info</button>
-        {toast && <Toast type={toastType} title={title} description={message} position={position} />}
+        <button class="btn success" onClick={() => toastHandler('success', 'bottomup', 'Success', 'Dunder Mifflin', 1500)}>success</button>
+        <button class="btn error" onClick={() => toastHandler('error', 'topdown', 'Failed', 'Battlestart Galatica', 1500)}>error</button>
+        <button class="btn warning" onClick={() => toastHandler('warning', 'bottomright', 'Warning', 'Wooooof', 1500)} >warning</button>
+        <button class="btn info" onClick={() => toastHandler('info', 'bottomleft', 'Info', 'Please note', 1500)}>info</button>
+
+        {toast && <Toast type={toastType} title={title} description={message} position={position} duration={duration} />}
       </header>
     </div>
   );
