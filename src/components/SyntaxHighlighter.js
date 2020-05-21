2727250ac7cee55b7d2a06f5e45ccfbc73fdc1cf
Ajin Kabeer <ajiinkabeer@gmail.com>
2020-05-21 12:32:54 +0530

    Bump react-lite-toast version

--- src/components/SyntaxHighlighter.js
+++ src/components/SyntaxHighlighter.js
@@ -1,13 +1,18 @@
-import React from 'react'
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { atomDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import React from "react";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 const Syntax = ({ code }) => {
-    return (
-        <SyntaxHighlighter language="javascript" style={atomDark} customStyle={{ width: '80%' }}>
-            {code || `<Toast type="success" title="Success" description="Dunder Mifflin" position="bottomup" duration={1500} />`}
-        </SyntaxHighlighter>
-    )
-}
+  return (
+    <SyntaxHighlighter
+      language="javascript"
+      style={atomDark}
+      customStyle={{ width: "75%" }}
+    >
+      {code ||
+        `<Toast type="success" title="Success" description="Dunder Mifflin" position="bottomup" duration={1500} />`}
+    </SyntaxHighlighter>
+  );
+};
 
-export default Syntax
+export default Syntax;
